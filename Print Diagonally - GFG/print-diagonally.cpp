94f1fceaf39d4811574c9c51831c3ab8013f32e3
Prashant Kumar Singh <95953965+PrashantA7985@users.noreply.github.com>
2022-10-18 17:52:49 +0530

    Added solution - LeetHub

---
+++ Print Diagonally - GFG/print-diagonally.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	
+	
+	
+	public:
+	vector<int> downwardDigonal(int n, vector<vector<int>> A)
+	{
+	   vector<vector<int>>v(2*n-1);
+	   for(int i=0;i<n;i++){
+	       for(int j=0;j<n;j++){
+	               v[i+j].push_back(A[i][j]);
+	              
+	           }
+	       }
+	   
+	   vector<int>ans;
+	   for(int i=0;i<2*n-1;i++){
+	       for(int j=0;j<v[i].size();j++)
+	       ans.push_back(v[i][j]);
+	   }
+	   
+	   return ans;
+		// Your code goes here
+
+	}
+
+};
+
+//{ Driver Code Starts.
+
+
+
+int main()
+{
+
+    
+    int t;
+    cin >> t;
+    while(t--) 
+    {
+        int n;
+        cin >> n;
+
+        vector<vector<int>> A(n, vector<int>(n));
+
+        for(int i = 0; i < n; i++)
+        	for(int j = 0; j < n; j++)
+        		cin >> A[i][j];
+
+        Solution obj;
+        vector<int> ans = obj.downwardDigonal(n, A);
+
+        for(auto i:ans)
+        	cout << i << " ";
+
+	    cout << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
