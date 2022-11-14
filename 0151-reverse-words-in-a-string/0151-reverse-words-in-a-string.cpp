f362be61808d00ef4029f9ef79ee917b7b9d1742
Prashant Kumar Singh <95953965+PrashantA7985@users.noreply.github.com>
2022-11-14 23:59:11 +0530

    Time: 15 ms (44.82%), Space: 7.4 MB (71.20%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
     string reverseWords(string s) {
-     string ans;
-        string r;
-        for(int i=s.size()-1;i>=0;i--){
-                if(s[i]!=' ')r.push_back(s[i]);
-                else {
-                    reverse(r.begin(),r.end());
-                    ans+=r;
-                    if(ans.size()!=0 && ans.back()!=' ')
-                    ans.push_back(' ');
-                    r="";
-                }
+     string ans,r;
+      stack<char>st;
+        for(int i=0;i<s.size();i++){
+              if(st.empty() && s[i]==' ')continue;
+               st.push(s[i]);
         }
-      
-        reverse(r.begin(),r.end());
-        
-        ans+=r;
-            string res;
-            int flag=0;
-            for(int i=ans.size()-1;i>=0;i--){
-                if(ans[i]==' ' && flag==0){}
-                else{
-                    res.push_back(ans[i]);
-                    flag=1;
-                }
+       
+        while(!st.empty()){
+             if(ans.size()==0 && st.top()==' '){}
+            else if(ans.back()==' ' && st.top()==' '){}
+              else  ans.push_back(st.top());
+            st.pop();
             
         }
-            reverse(res.begin(),res.end());
-        return res;
+     s=ans;
+        ans="";
+        for(int i=0;i<s.size();i++){
+             if(s[i]==' '){
+                  reverse(r.begin(),r.end());
+                 ans+=r;
+                 ans.push_back(' ');
+                 r="";
+             }
+            else r.push_back(s[i]);
+        }
+        reverse(r.begin(),r.end());
+        ans+=r;
+        return ans;
+     
         
     }
 };
