ec4233e009d604adb659ea877531c3e102c146db
Prashant Kumar Singh <95953965+PrashantA7985@users.noreply.github.com>
2022-11-15 20:22:15 +0530

    Added solution - LeetHub

--- Search insert position of K in a sorted array - GFG/search-insert-position-of-k-in-a-sorted-array.cpp
+++ Search insert position of K in a sorted array - GFG/search-insert-position-of-k-in-a-sorted-array.cpp
@@ -14,7 +14,7 @@ class Solution{
         while(low<=high){
             int mid=(low+high)/2;
             if(arr[mid]==k){
-                high=mid-1;
+             return mid;
             }
             else if(arr[mid]>k)high=mid-1;
             else low=mid+1;
