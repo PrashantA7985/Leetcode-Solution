a24036b719884264a6ddb91892f211bffd9ba4a0
Prashant Kumar Singh <95953965+PrashantA7985@users.noreply.github.com>
2022-11-15 20:06:28 +0530

    Time: 15 ms (65.21%), Space: 13.7 MB (18.70%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,10 +1,41 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& arr, int x) {
-  int z= lower_bound(arr.begin(),arr.end(),x)-arr.begin();
-        int y= upper_bound(arr.begin(),arr.end(),x)-arr.begin();
-        y--;
+         int z=INT_MAX;
+        int y=0;
+        int low=0;
+        int high=arr.size()-1;
         vector<int>ans;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==x){
+                z=min(mid,z);
+                 high=mid-1;     
+            }
+            else if(arr[mid]>x)high=mid-1;
+            else low=mid+1;
+            
+            
+        }
+        low=0;
+        high=arr.size()-1;
+                while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==x){
+                y=max(mid,y);
+                 low=mid+1;     
+            }
+            else if(arr[mid]>x)high=mid-1;
+            else low=mid+1;
+            
+            
+        }
+
+        
+        
+        
+        
+        
         if(z>y){
             ans.push_back(-1);
             ans.push_back(-1);
