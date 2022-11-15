3e8bc52b29823bea251d05c3fccdfe51b4afc085
Prashant Kumar Singh <95953965+PrashantA7985@users.noreply.github.com>
2022-11-16 00:41:59 +0530

    Added solution - LeetHub

--- Sum of Beauty of All Substrings - GFG/sum-of-beauty-of-all-substrings.cpp
+++ Sum of Beauty of All Substrings - GFG/sum-of-beauty-of-all-substrings.cpp
@@ -9,27 +9,26 @@ using namespace std;
 class Solution {
   public:
     int beautySum(string s) {
-      int ans=0;
+       int ans=0;
         for(int i=0;i<s.size();i++){
-              vector<int>m(26);
-            for(int k=0;k<26;k++)m[k]=-1;
-             int x=0;
+          vector<int>v(26);
+          for(int i=0;i<26;i++)v[i]=-1;
+             
+                int x=0;
             for(int j=i;j<s.size();j++){
-              m[int(s[j]-'a')]++;
-              int y=INT_MAX;
+              v[s[j]-'a']++;
+                x=max(x,v[s[j]-'a']+1);
+             int y=INT_MAX;
+               for(auto it:v){
+                   if(it!=-1 && it!=x-1)y=min(y,it+1);
+               }
+             
                
-              x=max(m[int(s[j]-'a')]+1,x);
-                for(int k=0;k<26;k++){
-                    if(m[k]!=x && m[k]!=-1){
-                        y=min(m[k]+1,y);
-                    }
-                }
              
                 
-                
-           
-                        
-                if( y!=0)ans+=x-y;
+        
+            
+                if(x>y && y>0)ans+=x-y;
             }
         
         }
