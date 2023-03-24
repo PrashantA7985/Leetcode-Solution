cd13d595e7c3fa1a0f5cf52a23b08e77ce96bfda
Prashant Kumar Singh <95953965+PrashantA7985@users.noreply.github.com>
2023-03-24 19:32:31 +0530

    Time: 606 ms (26.41%), Space: 126.8 MB (32.42%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int minReorder(int n, vector<vector<int>>& v) {
+         int vis[n+1];
+        for(int i=0;i<=n;i++)vis[i]=0;
+         vector<vector<int>>adj(n+1);
+        set<pair<int,int>>s;
+        
+        // for(int i=0;i<n;i++){
+        //     adj[v[i][0]].push_back(v[i][1]);
+        //         adj[v[i][1]].push_back(v[i][0]);
+        //         s.insert({v[i][0],v[i][1]});
+        // }
+         for (auto c : v){
+            adj[c[0]].push_back(c[1]);
+            adj[c[1]].push_back(c[0]);
+             s.insert({c[0],c[1]});
+        }
+        queue<int>q;
+        q.push(0);
+        int ans=0;
+        // vis[0]=1;
+        while(!q.empty()){
+              
+             int temp=q.front();
+              q.pop();
+              vis[temp]=1;
+              for(auto it:adj[temp]){
+                  if(vis[it]==0)q.push(it);
+                  if(vis[it]==0 && s.count({temp,it})){
+                      // cout<<temp<<" "<<it<<endl;
+                      // q.push(it);
+                      ans++;
+                    
+                  }
+              }
+            
+        }
+        return ans;
+        
+    }
+};
