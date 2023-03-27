48e0fce548ce491a2a3f4e7bb268319801aed078
Prashant Kumar Singh <95953965+PrashantA7985@users.noreply.github.com>
2023-03-27 11:08:33 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (99.95%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n<=1)return n;
+        int x=0;
+        int y=1;
+        int ans;
+        for(int i=2;i<=n;i++){
+            ans=x+y;
+            x=y;
+            y=ans;
+        }
+        return ans;
+           
+    }
+};
