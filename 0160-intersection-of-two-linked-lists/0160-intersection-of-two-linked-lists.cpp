b3187dbf6f13e64cff87bc158031196d9dc58712
Prashant Kumar Singh <95953965+PrashantA7985@users.noreply.github.com>
2023-04-12 11:58:53 +0530

    Time: 39 ms (95.73%), Space: 14.4 MB (90.79%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -11,21 +11,43 @@ public:
     ListNode *getIntersectionNode(ListNode *h1, ListNode *h2) {
             ListNode *d1=h1;
             ListNode * d2=h2;
-        int flag=0;
-          while(flag<=2){
-              if(d1==NULL){
-                  d1=h2;
-                  flag++;
-              }
-              if(d2==NULL){
-                  d2=h1;
-                  flag++;
-              } if(d1==d2)return d1;
-              
-              d1=d1->next;
-              d2=d2->next;
-             
-          }
+            int c1=0;
+        int c2=0;
+             while(d1){
+                 c1++;
+                 d1=d1->next;
+             }
+                while(d2){
+                 c2++;
+                 d2=d2->next;
+             }
+        if(c1>=c2){
+            int x=c1-c2;
+            d1=h1;
+            for(int i=0;i<x;i++){
+                d1=d1->next;
+            }
+            d2=h2;
+            while(d1!=NULL && d2!=NULL){
+                 if(d1==d2)return d1;
+                d1=d1->next;
+                d2=d2->next;
+            }
+        }
+            else{
+                int x=c2-c1;
+            d2=h2;
+            for(int i=0;i<x;i++){
+                d2=d2->next;
+            }
+            d1=h1;
+            while(d2!=NULL && d1!=NULL){
+                 if(d1==d2)return d1;
+                d1=d1->next;
+                d2=d2->next;
+            } 
+            }
+          
         return NULL;
     }
 };
