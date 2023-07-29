1e558d1e912b1fd514de306707a1c22a93ac2bad
Prashant Kumar Singh <95953965+PrashantA7985@users.noreply.github.com>
2023-07-29 11:16:38 +0530

    Time: 72 ms (54.65%), Space: 24.5 MB (25.37%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-   int lcsUtil(string& s1, string& s2, int ind1, int ind2,  vector<vector<int>>& dp){
-
-    if(ind1<0 || ind2<0)
+    int f(string & s1, string & s2,int i,int j, vector<vector<int>>&v){
+            if(i<0 || j<0)
         return 0;
         
-    if(dp[ind1][ind2]!=-1)
-        return dp[ind1][ind2];
+    if(v[i][j]!=-1)
+        return v[i][j];
     
-    if(s1[ind1] == s2[ind2])
-        return dp[ind1][ind2] = 1 + lcsUtil(s1,s2,ind1-1,ind2-1,dp);
+    if(s1[i] == s2[j])
+        return v[i][j] = 1 + f(s1,s2,i-1,j-1,v);
     
     else 
-        return dp[ind1][ind2] = 0 + max(lcsUtil(s1,s2,ind1,ind2-1,dp),lcsUtil
-       (s1,s2,ind1-1,ind2,dp));
-   
-}
+        return v[i][j] = 0 + max(f(s1,s2,i,j-1,v),f(s1,s2,i-1,j,v));
 
+    }
     int longestCommonSubsequence(string s1, string s2) {
-int n=s1.size();
-    int m=s2.size();
-
-    vector<vector<int>> dp(n,vector<int>(m,-1));
-    return lcsUtil(s1,s2,n-1,m-1,dp);
-
+        int n=s1.size();
+        int m=s2.size();
+        vector<vector<int>> v(n,vector<int>(m,-1));
+             // for(int i=0;i<n;i++){
+             //     for(int j=0;j<m;j++)v[i][j]=-1;
+             // }                
+        return f(s1,s2,n-1,m-1,v);
     }
 };
