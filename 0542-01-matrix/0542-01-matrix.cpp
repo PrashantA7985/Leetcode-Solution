111311a3509bcce3aeb93cb8c6e4dbf81754fa67
Prashant Kumar Singh <95953965+PrashantA7985@users.noreply.github.com>
2023-08-17 20:09:33 +0530

    Time: 68 ms (82.37%), Space: 34.6 MB (17.65%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -3,38 +3,34 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& grid) {
         int n=grid.size();
 	     int m=grid[0].size();
-	    int vis[n][m];
-	    vector<vector<int>>ans(n,vector<int>(m,0));
-	    queue<pair<pair<int,int>,int>>q;
-	    for(int i=0;i<n;i++){
-	        for(int j=0;j<m;j++){
-	            if(grid[i][j]==0){
-	                vis[i][j]=1;
-	                q.push({{i,j},0});
-	               
-	            }
-	            else vis[i][j]=0;
-	        }
-	    }
-	     int a[]={-1,0,1,0};
-	     int b[]={0,1,0,-1};
-	    while(!q.empty()){
-	        int r=q.front().first.first;
-	        int c=q.front().first.second;
-	        int t=q.front().second;
-	        ans[r][c]=t;
-	        q.pop();
-	        for(int i=0;i<4;i++){
-	            int nrow=r+a[i];
-	            int ncol=c+b[i];
-	            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && 
-	            vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
-	                q.push({{nrow,ncol},t+1});
-	                vis[nrow][ncol]=1;
-	            }
-	        }
-	        
-	    }
-	    return ans;
+     vector<vector<int>>vis(n,vector<int>(m,0));
+        vector<vector<int>>ans(n,vector<int>(m,0));
+        queue<pair<pair<int,int>,int>>q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==0){
+                    q.push({{i,j},0});
+                    vis[i][j]=1;
+                }
+            }
+        }
+        int a[]={-1,0,1,0};
+        int b[]={0,1,0,-1};
+        while(!q.empty()){
+            int row=q.front().first.first;
+            int col=q.front().first.second;
+            int step=q.front().second;
+            ans[row][col]=step;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nrow=row+a[i];
+                int ncol=col+b[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0){
+                    vis[nrow][ncol]=1;
+                    q.push({{nrow,ncol},step+1});
+                }
+            }
+        }
+        return ans;
     }
 };
