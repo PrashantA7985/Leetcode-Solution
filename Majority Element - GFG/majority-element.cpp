ecc73508737264e8820173f2a91f3ccfe2c97af9
Prashant Kumar Singh <95953965+PrashantA7985@users.noreply.github.com>
2023-10-02 11:59:20 +0530

    Added solution - LeetHub

--- Majority Element - GFG/majority-element.cpp
+++ Majority Element - GFG/majority-element.cpp
@@ -17,12 +17,23 @@ class Solution{
     {
         
         // your code here
-        map<int,int>m;
-        for(int i=0;i<size;i++)m[a[i]]++;
-        for(auto it:m){
-            if (2*it.second>size)return it.first;
+        int ans=a[0];
+        int count=1;
+        for(int i=1;i<size;i++){
+            if(ans==a[i])count++;
+            else count--;
+            if(count==0){
+                ans=a[i];
+                count=1;
+            }
         }
-        return -1;
+        count=0;
+       for(int i=0;i<size;i++){
+           if(ans==a[i])count++;
+       }
+       if(count>size/2)return ans;
+       return -1;
+       
         
     }
 };
