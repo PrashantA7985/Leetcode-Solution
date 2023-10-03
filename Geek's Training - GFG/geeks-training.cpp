011d78d80750d14835570d87de6e2aa2356f2bc6
Prashant Kumar Singh <95953965+PrashantA7985@users.noreply.github.com>
2023-10-03 19:10:01 +0530

    Added solution - LeetHub

--- Geek's Training - GFG/geeks-training.cpp
+++ Geek's Training - GFG/geeks-training.cpp
@@ -4,31 +4,28 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-         int   solve(vector<vector<int>>& v,int n,int last,vector<vector<int>>& dp){
-                if(n==0){
-                    int maxi=0;
-                    for(int i=0;i<3;i++){
-                        if(i!=last)maxi=max(maxi,v[n][i]);
-                    }
-                    return maxi;
-                } 
-                 if(dp[n][last]!=-1)return dp[n][last];
-                  int maxi=0;
-             
-                  for(int i=0;i<3;i++){
-                      if(i!=last){
-                          int temp=v[n][i]+solve(v,n-1,i,dp);
-                           maxi=max(maxi,temp);
-                      }
-                  }
-                  return dp[n][last]=maxi;
-            }
   public:
+    int solve(vector<vector<int>>&v,  int i,int x,vector<vector<int>>&dp){
+        if(i==0){
+            int ans=0;
+            for(int j=0;j<3;j++){
+                 if(j!=x)ans=max(ans,v[i][j]);
+            }
+            return ans;
+        }
+        if(dp[i][x]!=-1)return dp[i][x];
+        int ans=0;
+        for(int j=0;j<3;j++){
+            if(j!=x){
+               ans=max(ans, v[i][j]+solve(v,i-1,j,dp));
+            }
+        }
+        return dp[i][x]=ans;
+    }
     int maximumPoints(vector<vector<int>>& v, int n) {
         // Code here
         vector<vector<int>>dp(n,vector<int>(4,-1));
-         int ans=solve(v,n-1,3,dp);
-         return ans;
+        return solve(v,n-1,3,dp);
     }
 };
 
